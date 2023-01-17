2c17e0f174ebfb7d34ee00bdda769f1768a79201
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-17 01:18:32 -0800

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     result = ""
     for character in str:
         if 97 <= ord(character) <= 122:
-            ord(character) -= 32
+            character = chr(ord(character) - 32)
         result += character
     print("{}".format(result))
-
