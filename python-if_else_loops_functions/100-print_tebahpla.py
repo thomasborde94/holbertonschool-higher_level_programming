cad4581996221b08509a96945bfc72a648309db8
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-17 02:42:03 -0800

    task 100

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_alpha():
+    result = ""
+    for i in range(123, 97):
+        if i % 2 == 0:
+            i = chr(ord(i) - 32)
+        result += i
+        print("{}".format(result), end="")
