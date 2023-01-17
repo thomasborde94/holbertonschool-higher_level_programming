b10c35e072608889c8c7589e9ec8b3a6a31cd325
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-17 02:25:41 -0800

    task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,9 +5,7 @@ def fizzbuzz():
             print("FizzBuzz ", end="")
         elif i % 3 == 0:
             print("Fizz ", end="")
-        elif ((i % 5 == 0) and (i != 100)):
+        elif ((i % 5 == 0):
             print("Buzz ", end="")
-        elif ((i % 5 == 0) and (i == 100)):
-            print("Buzz", end="")
         else:
             print("{} ".format(i), end="")
