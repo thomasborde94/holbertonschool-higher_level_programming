1c24a5a974549f35f055dc17374d9e9836355deb
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-18 02:16:36 -0800

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
 def infinite_add():
-    result = 0
-    if len(sys.argv) == 1:
-        print("0")
-    else:
-        for i in range(1, len(sys.argv)):
-            result += int(sys.argv[i])
-            i += 1
-        print("{}".format(result))
+    if __name__ == "__main__":
+        result = 0
+        if len(sys.argv) == 1:
+            print("0")
+        else:
+            for i in range(1, len(sys.argv)):
+                result += int(sys.argv[i])
+                i += 1
+                print("{}".format(result))
 
 infinite_add()
