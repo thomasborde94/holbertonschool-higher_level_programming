cdda817564faa43b07a04eb14e18986731764974
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-23 04:16:40 -0800

    task 9 correction

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,11 @@
 
 
 def max_integer(my_list=[]):
-    x = 0
     if len(my_list) == 0:
         return None
-    for i in range(1, len(my_list)):
-        if my_list[i] > my_list[i - 1] and my_list[i] > x:
-            x = my_list[i]
-    return x
+    max_val = my_list[0]
+ 
+    for i in range(len(my_list)):
+        if my_list[i] > max_val:
+            max_val = my_list[i]
+    return max_val
