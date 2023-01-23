f78d08b32c3fca231c153341b32dd6a5c4eed5e2
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-23 04:00:53 -0800

    8 correct

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@
 def multiple_returns(sentence):
     length = len(sentence)
     if length == 0:
-        sentence[0] = None
+        tuple = length, None
     else:
         tuple = length, sentence[0]
     return tuple
