07b4928872b9e0f0deada5b5f87cd36b1edb76eb
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-23 02:11:29 -0800

    changed 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -7,7 +7,7 @@ def no_c(my_string):
     else:
         newString = ""
         for i in range(0, len(my_string)):
-            if my_string[i] != "c" and my_string[i] != "C"
-            and my_string[i] is not None:
+            y = my_string[i]
+            if y != "c" and y != "C" and y is not None:
                 newString += my_string[i]
         return newString
