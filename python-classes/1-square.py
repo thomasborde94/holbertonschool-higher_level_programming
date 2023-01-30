10fa0bcdbeb8064c7e9c7591e1d305209b6ec715
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-30 02:47:15 -0800

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""Defines the classs Square"""
+class Square:
+    def __init__(self, size):
+
+        """
+        Args :
+        size (int): Size of a new square
+        """
+        self.__size = size
