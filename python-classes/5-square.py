b695037598463204728708f3bff36960e24c53ca
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-30 04:29:01 -0800

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Defines the class Square"""
+
+
+class Square:
+    """represents a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+        Args:
+        size (int): size a the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """returns area of square"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                print("#" * self.size)
