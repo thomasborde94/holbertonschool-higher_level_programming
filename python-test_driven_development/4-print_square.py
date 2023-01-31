863ab5e23b3446f67ddbd0a045778c43df2af1ee
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-31 05:25:08 -0800

    task 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""prints a square with #
+"""
+
+
+def print_square(size):
+    """
+    prints a square of size size with #
+    Args:
+        size (int): first parameter
+    Return: nothing
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    
+    for i in range(size):
+        print("#" * size)
