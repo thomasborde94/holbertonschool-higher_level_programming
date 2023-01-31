f1a71068af822d75813b6f65d7104f844dd1f63b
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-31 05:47:13 -0800

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,9 +12,12 @@ specific characters
     """
     if type(text) != str:
         raise TypeError("text must be a string")
-    for i in text:
-        if i != "." and i != "," and i != "?" and i != ":":
-            print("{}".format(i), end="")
-        else:
-            print("{}".format(i))
+    a = 0
+    while a < len(text):
+        if text[a] in [':', '.', '?']:
+            print(text[a])
             print()
+            a += 1
+        else:
+            print(text[a], end='')
+            a += 1
