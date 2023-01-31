d33d7141ff17261cbeda6abe09ccc218129eef7b
Thomas Borde <thomas.borde94@hotmail.fr>
2023-01-31 01:31:45 -0800

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines an integer adition function
+"""
+
+
+
+def add_integer(a, b=98):
+    """
+    Adds a and b and returns sum
+    Args:
+        a (int): first parameter
+        b (int): second parameter
+    Return:
+        sum of a + b
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
