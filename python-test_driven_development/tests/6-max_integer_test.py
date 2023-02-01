c4a8f3ea1897520ec0c6b1389294022d6e34faed
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-01 00:38:05 -0800

    task 6

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Unittest for max integer
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+        """class for cases"""
+
+        def test_cases(self):
+            """test cases for function"""
+            self.assertEqual(max_integer([13, 0, 56, 3]), 56)
+            self.assertEqual(max_integer([-3, -6, -1, -67]), -1)
+            self.assertEqual(max_integer(), None)
+            self.assertEqual(max_integer([0, 0, 0]), 0)
+            self.assertEqual(max_integer([-5, 0, 6]), 6)
+            self.assertEqual(max_integer([73210584, 58, -68, 3]), 73210584)
+            self.assertEqual(max_integer([1, 6]), 6)
+            self.assertEqual(max_integer([87]), 87)
+            self.assertEqual(max_integer([6, 8, 10e1000]), float('inf'))
+            self.assertEqual(max_integer([87, float('nan'), 5]), 87)
+            self.assertEqual(max_integer("Holberton"), 't')
+
+if __name__ == '__main__':
+    unittest.main()
