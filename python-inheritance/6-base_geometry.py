770aa8bd706bd6a38ad79516efe5bbfdc9abe6cf
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-07 05:25:30 -0800

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines BaseGeometry class"""
+
+
+class BaseGeometry:
+    """defines BaseGeometry class"""
+    def area(self):
+        raise Exception("area is not implemented")
