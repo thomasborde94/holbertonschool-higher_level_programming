b4c315675484e2320b1f637ed06b29375ea5f174
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-07 04:55:00 -0800

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """returns true if obj is instance of class"""
-    if isinstance(obj, a_class):
+    if type(obj) is a_class:
         return True
     else:
         return False
