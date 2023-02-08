80187a1a01bc8541d771f3f14a46b10836280c46
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-08 04:31:32 -0800

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""defines BaseGeometry and Rectangle classes"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """defines Rectangle which inherits from basegeo"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        area = self.__width * self.__height
+        return area
+
+    def __str__(self):
+        string = "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return string
+
+class Square(Rectangle):
+    """defines Square which inherits from rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
