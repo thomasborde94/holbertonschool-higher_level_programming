a1c532bd0705c2706baf535e3167620628a4f43b
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-08 05:11:02 -0800

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines read_file function"""
+
+
+def read_file(filename=""):
+    """reads a text file and prints it"""
+    with open(filename, "r", encoding="utf-8") as myFile:
+        print(myFile.read())
