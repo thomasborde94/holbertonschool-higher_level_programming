f9e988f0d1e703036b90042f701b4eb4cf003c93
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-09 02:25:44 -0800

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student():
+    """defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list):
+            dict = {}
+            for key, value in self.__dict__.items():
+                for el in attrs:
+                    if key == el:
+                        dict[key] = value
+            return dict
+        else:
+            return self.__dict__
+
+        def reload_from_json(self, json):
+            for key, value in json.items():
+                setattr(self, key, value)
