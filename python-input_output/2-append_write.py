e335ed0590797ea77567420d55d27b5e25ae0ba0
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-09 00:05:16 -0800

    task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines append_write function"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, "a", encoding="utf-8") as myFile:
+        myFile.write(text)
+    return len(myFile)
