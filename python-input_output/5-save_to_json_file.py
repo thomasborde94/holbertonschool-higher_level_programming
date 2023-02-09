069cc22410df12b981c80521e8f7ca104b1c8698
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-09 00:25:07 -0800

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
-    json_object = json.dumps(my_obj)
     with open(filename, "w") as myFile:
-        myFile.write(json_object)
+        myFile.write(json.dumps(my_obj))
