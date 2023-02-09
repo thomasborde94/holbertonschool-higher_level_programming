efe412b618810c689bd43c4cf8153d2fdad9954d
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-09 01:08:34 -0800

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,13 +7,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_item():
-    """adds all arguments to a Python list, \
-    and then save them to a file"""
-    args = sys.argv
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        my_list = []
-    my_list.extend(argv[1:])
-    save_to_json_file(my_list, "add_item.json")
+args = sys.argv
+try:
+    my_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    my_list = []
+my_list.extend(argv[1:])
+save_to_json_file(my_list, "add_item.json")
