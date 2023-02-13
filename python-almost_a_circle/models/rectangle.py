d6c9ee1e71e831d2564a294f585a4270a4f8800f
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-13 01:52:31 -0800

    task 5

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,10 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,\
+                                                       self.__y, self.__width,\
+                                                       self.__height)
     def area(self):
         """return area of the Rectangle"""
         return self.__width * self.__height
