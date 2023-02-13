a8c0f281080286a46f9f64a81cfede0d89eea4f9
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-13 03:34:21 -0800

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,6 +14,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns json string repr of dict"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
