01ca42537c11d64b1d4c63c0744edc513f5d0712
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-13 02:53:21 -0800

    task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,3 +23,28 @@ class Square(Rectangle):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.width = value
+
+    def update(self, *args, **kwargs):
+        """update the rectangle id, width, height, x and y"""
+        if len(args) != 0 and args is not None:
+            count = 0
+            for arg in args:
+                if count == 0:
+                    self.id = arg
+                elif count == 1:
+                    self.width = arg
+                elif count == 2:
+                    self.x = arg
+                elif count == 3:
+                    self.y = arg
+                count += 1
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.width = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
