d9d1feb972c15ff4f4d550da271c6c0462372636
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-13 01:43:36 -0800

    task 4

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,6 +12,9 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    def area(self):
+        return self.__width * self.__height
+
     @property
     def width(self):
         return self.__width
