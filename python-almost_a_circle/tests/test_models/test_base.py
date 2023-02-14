b99152c27ddaedab0a5a9d9db158d370766661b3
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-14 02:11:00 -0800

    added test to base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -19,5 +19,12 @@ class TestBase(unittest.TestCase):
         baseTest2 = Base()
         self.assertEqual(baseTest2.id, 2)
 
+    def test_to_json_string(self):
+        """test of to_json_string func"""
+        dic = {"id": 1, "x": 2, "y": 3, "width": 4, "height": 5}
+        json_dic = Base.to_json_string([dic])
+        self.assertTrue(isinstance(dic, dict))
+        self.assertTrue(isinstance(json_dic, str))
+
 if __name__ == "__main__":
     unittest.main()
