5eb90f7d1a5f08cc54e15ba27328caaccd646663
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-14 01:58:11 -0800

    added square tests

---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"Unit tests for Rectangle class"
+import unittest
+from models.square import Square
+from models.rectangle import Rectangle
+
+
+class TestSquare(unittest.TestCase):
+    """Testing Square"""
+
+    def test_instance(self):
+        """test input size correct standard """
+        s = Square(5)
+        self.assertEqual(s.width, 5)
+        self.assertEqual(s.height, 5)
+
+        with self.assertRaises(TypeError):
+            Square(5, "1")
+
+        with self.assertRaises(TypeError):
+            Square()
+
+        with self.assertRaises(TypeError):
+            Square("1")
+
+        with self.assertRaises(ValueError):
+            Square(-5, 3, 4)
+
+        with self.assertRaises(TypeError):
+            Square(1, 2, "3")
+
+        with self.assertRaises(ValueError):
+            Square(1, -2)
+
+        with self.assertRaises(ValueError):
+            Square(1, 2, -3)
+
+        with self.assertRaises(ValueError):
+            Square(0)
+
+    def test_case_normal(self):
+        """Test of Square(1, 2, 3, 4) exists"""
+        s = Square(1, 2, 3, 4)
+        self.assertEqual(s.id, 4)
+        self.assertEqual(s.size, 1)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
+
+    def test_area(self):
+        """testing area"""
+        s = Square(5)
+        self.assertEqual(s.area(), 25)
+        s = Square(1, 2)
+        self.assertEqual(s.area(), 1)
+
+    def test_string(self):
+        """Test str"""
+        s2 = Square(1, 2, 5, 12)
+        self.assertEqual(str(s2), "[Square] (12) 2/5 - 1")
+
+if __name__ == "__main__":
+    unittest.main()
