3363aa1f70b63ea0cb47c9fd971212ae9e0d9cdc
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-14 00:09:04 -0800

    first commit of baseTest file

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"Unit tests for Base class"
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """tests for class Base"""
+
+    def test_correctId(self):
+        """assigns the correct id when it is given"""
+        baseTest = Base(10)
+        self.assertEqual(baseTest.id, 10)
+
+    def test_autoId(self):
+        """assigns an id automatically when id is not given"""
+        baseTest = Base()
+        self.assertEqual(baseTest.id, 1)
+
+if __name__ == "__main__":
+    unittest.main()
