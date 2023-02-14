736d4bc5a81d353e343fd04fcfaab89c4a8a2a92
Thomas Borde <thomas.borde94@hotmail.fr>
2023-02-14 00:43:28 -0800

    added tests for rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -15,3 +15,37 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r.y, 0)
         r2 = Rectangle(10, 5, 0, 0, 8)
         self.assertEqual(r2.id, 8)
+
+    def test_init(self):
+        """test with given properties"""
+        r = Rectangle(10, 5, 2, 1)
+        self.assertEqual(r.width, 10)
+        self.assertEqual(r.height, 5)
+        self.assertEqual(r.x, 2)
+        self.assertEqual(r.y, 1)
+        
+    def test_notInteger(self):
+        """test when args are not ints"""
+        self.assertRaisesRegex(
+            TypeError, "width must be an integer", Rectangle, "1", 2)
+        self.assertRaisesRegex(
+            TypeError, "height must be an integer", Rectangle, 1, "2")
+        self.assertRaisesRegex(
+            TypeError, "x must be an integer", Rectangle, 1, 2, "3")
+        self.assertRaisesRegex(
+            TypeError, "y must be an integer", Rectangle, 1, 2, 3, "4")
+
+    def test_valueError(self):
+        """tests when is int but no valid values"""
+        self.assertRaisesRegexr(
+            ValueError, "width must be > 0", Rectangle, -5, 9)
+        self.assertRaisesRegex(
+            ValueError, "height must be > 0", Rectangle, 5, -9)
+        self.assertRaisesRegex(
+            ValueError, "width must be > 0", Rectangle, 0, 9)
+        self.assertRaisesRegex(
+            ValueError, "height must be > 0", Rectangle, 5, 0)
+        self.assertRaisesRegex(
+            ValueError, "x must be >= 0", Rectangle, 1, 2, -3)
+        self.assertRaisesRegex(
+            ValueError, "y must be >= 0", Rectangle, 1, 2, 3, -4)
