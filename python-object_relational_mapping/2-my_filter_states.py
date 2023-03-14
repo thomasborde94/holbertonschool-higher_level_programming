df021faff5da55d68aaba9cb1663930ed659fe9b
Thomas Borde <thomas.borde94@hotmail.fr>
2023-03-14 03:54:56 -0700

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument."""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    statename = sys.argv[4]
+    connection = MySQLdb.connect(user = username, passwd = password,
+                                 db = database, host = "localhost",
+                                 port = 3306)
+    cursor = connection.cursor()
+    request = "SELECT * FROM states WHERE BINARY 'name' = '{}' ORDER BY states.id ASC".format(statename)
+    cursor.execute(request)
+
+    stateList = cursor.fetchall()
+
+    for state in stateList:
+        print(state)
+
+    cursor.close()
+    connection.close()
