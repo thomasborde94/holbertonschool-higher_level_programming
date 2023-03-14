0a6e1185e24fa9d8d6dbeb897a824df6ca3cd99d
Thomas Borde <thomas.borde94@hotmail.fr>
2023-03-14 03:42:37 -0700

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,12 +8,12 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    connection = MySQLdb.connect(user = username, passwd = password,
-                                 db = database, host = "localhost",
-                                 port = 3306)
+    connection = MySQLdb.connect(user=username, passwd=password,
+                                 db=database, host="localhost",
+                                 port=3306)
 
     cursor = connection.cursor()
-    request = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id"
+    request = "SELECT * FROM states WHERE name LIKE '%N%' ORDER BY states.id"
     cursor.execute(request)
 
     stateList = cursor.fetchall()
