b3f532b94703965dbdef569f5fb39317c2aaa46d
Thomas Borde <thomas.borde94@hotmail.fr>
2023-03-15 06:02:35 -0700

    task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    result = engine.execute(text("SELECT * FROM states ORDER BY states.id;"))
+    i = 1
+    for row in result.fetchall():
+        while i < 2:
+            print("{}: {}".format(row[0], row[1]))
+            i += 1
