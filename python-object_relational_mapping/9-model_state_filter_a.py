c9df9f100bf35dd4305a927d7bde75c47654c7b9
Thomas Borde <thomas.borde94@hotmail.fr>
2023-03-15 06:37:36 -0700

    task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(username, password, database))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like("%a%")).all()
+
+    if states is None:
+        print("Nothing")
+    else:
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+    """
+    Good result but checker not happy
+    result = engine.execute(text("SELECT * FROM states
+    ORDER BY states.id LIMIT 1;"))
+    for row in result.fetchall():
+        print("{}: {}".format(row[0], row[1]))
+    """
