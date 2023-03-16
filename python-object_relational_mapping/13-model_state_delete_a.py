bb23bcac4e86173f7f02989dfa3fada4529a6ae7
Thomas Borde <thomas.borde94@hotmail.fr>
2023-03-16 03:25:18 -0700

    task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-prints the State object with the name passed as argument
+deletes all State objects with a name containing the letter a
 """
 from model_state import Base, State
 import sys
