9c8ea963728efa11d915bb2ef651360e44948dc0
Thomas Borde <thomas.borde94@hotmail.fr>
2023-04-09 01:15:16 -0700

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ const number = parseInt(process.argv[2]);
 if (isNaN(number)) {
   console.log('Missing size');
 } else {
-    for (let i = 1; i <= number; i++) {
-	console.log('X'.repeat(number));
-    }
+  for (let i = 1; i <= number; i++) {
+    console.log('X'.repeat(number));
+  }
 }
